54dc38531449fd4e83802674e89900a3a7b4182f
melon <2402994714@qq.com>
2020-09-11 11:09:48 +0800

    冲突

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 //品牌管理
-=======
 //  后台首页.品牌
->>>>>>> 86201ed3dc5956bfa42711c431d6ffae0777ea80
 Route::get('/brand/brand','Admin\BrandController@create');
 Route::post('/brand/store','Admin\BrandController@store');
 Route::get('/brand/index','Admin\BrandController@index');
@@ -30,7 +27,6 @@ Route::post('/brand/update/{id}','Admin\BrandController@update');
 Route::get('/brand/destroy/{id?}','Admin\BrandController@destroy');
 Route::get('/brand/change','Admin\BrandController@change');
 
-<<<<<<< HEAD
 //分类管理
 Route::get('cate','Admin\CateController@index');//列表展示
 Route::get('/cate/create','Admin\CateController@create');//添加页面
@@ -38,7 +34,6 @@ Route::post('/cate/store','Admin\CateController@store');//执行添加
 Route::get('/cate/edit/{id}','Admin\CateController@edit');//编辑展示页面
 Route::post('/cate/update/{id}','Admin\CateController@update');//执行编辑
 Route::get('/cate/destroy/{id}','Admin\CateController@destroy');//删除
-=======
 //商品
 Route::prefix('student')->group(function() {
     Route::get('create', 'studentController@create');
@@ -51,4 +46,3 @@ Route::prefix('student')->group(function() {
 
 
 
->>>>>>> 86201ed3dc5956bfa42711c431d6ffae0777ea80
