5a41e8c50acf8e9c06ec406b43796f1b6bb6ede6
melon <2402994714@qq.com>
2020-09-11 20:27:28 +0800

    商品分类

--- resources/views/admin/cate/index.blade.php
+++ resources/views/admin/cate/index.blade.php
@@ -20,12 +20,28 @@
 			<td>{{$v->cate_shows}}</td>
 			<td>
 				<a href="{{url('/cate/edit/'.$v->cate_id)}}" class="btn btn-primary">编辑</a>
-				<a href="{{url('/cate/destroy/'.$v->cate_id)}}" class="btn btn-danger">删除</a>
+				<!-- <a href="{{url('/cate/destroy/'.$v->cate_id)}}" class="btn btn-danger">删除</a> -->
+
+				<a href="javascript:void(0)" onclick="deleteByID({{$v->cate_id}},this)" class="btn btn-danger">删除</a>
 			</td>
 		</tr>
 @endforeach
 	</tbody>
 </table>
+<script>
+	function deleteByID(cate_id,obj){
+		if(!cate_id){
+			return;
+		}
+		$.get('/cate/destroy/'+cate_id,function(res){
+			alert(res.msg);
+			$(obj).parents('tr').hide();
+			<!-- location.reload(); -->
+		},'json');
+	}
+	//ajax分页
+	
+</script>
 
 
 @endsection
