fd8c5b7ce92e13b86a0e47ad5e47060a746bff60
zhangxuepeng <3238811454@qq.com>
2020-09-12 15:53:41 +0800

    d

--- resources/views/admin/cate/create.blade.php
+++ resources/views/admin/cate/create.blade.php
@@ -11,7 +11,7 @@
   <div class="form-group">
     <label for="firstname" class="col-sm-2 control-label">分类名称</label>
     <div class="col-sm-10">
-      <input type="text" class="form-control" name="cate_name" id="firstname" 
+      <input type="text" class="form-control" name="cate_name" id="firstname"
            placeholder="请输入分类名称">
     </div>
   </div>
@@ -29,18 +29,18 @@
   <div class="form-group">
     <label for="lastname" class="col-sm-2 control-label">是否显示</label>
     <div class="col-sm-10">
-      <input type="radio" name="cate_show" value="1" checked>显示
-           <input type="radio" name="is_show" value="2">不显示
+      <input type="radio" name="cate_show" value="显示" checked>显示
+        <input type="radio" name="is_show" value="不显示">不显示
     </div>
   </div>
   <div class="form-group">
     <label for="lastname" class="col-sm-2 control-label">是否导航显示</label>
     <div class="col-sm-10">
-      <input type="radio" name="cate_shows" value="1" checked>显示
-           <input type="radio" name="is_nav_show" value="2">不显示
+      <input type="radio" name="cate_shows" value="显示" checked>显示
+           <input type="radio" name="is_nav_show" value="不显示">不显示
     </div>
   </div>
- 
+
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-default">提交</button>
