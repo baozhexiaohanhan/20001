4a08e6dc13f5f4c732f8898d62c1b730609d8e02
zhangxuepeng <3238811454@qq.com>
2020-09-16 11:21:36 +0800

    wwww

--- resources/views/admin/reg.blade.php
+++ resources/views/admin/reg.blade.php
@@ -1,74 +1,54 @@
-@extends('admin.layout.gong')
-@section('title', '注册')
-@section('content')
 
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<!-- <center><h1>注册<a style="float:right" href="{{url('/login')}}" type="button" class="btn btn-info">登录</a></h1></center><hr/> -->
-<header>
-    <!-- <a href="javascript:history.back(-1)" class="back-off fl"><span class="glyphicon glyphicon-menu-left"></span></a> -->
-    <a style="float:right" href="{{url('/login')}}" type="button" class="btn btn-info">登录</a>
-    <div class="head-mid">
-      <h1>会员注册</h1>
-    </div>
-</header>
-<div class="head-top">
-
-</div><!--head-top/-->
-<form action="{{url('/admin/Doreg')}}" method="post" class="reg-login" id="myform">
-    @csrf
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li style="color:red;">{{ $error }}</li>
-                @endforeach
-            </ul>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>dwCms后台管理系统-登录</title>
+    <link rel="stylesheet" type="text/css" href="./layui/css/layui.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/admin/css/login.css" />
+</head>
+<body class="beg-login-bg">
+    <div class="beg-login-box">
+        <header>
+            <h1><font color="red">注册</font>></h1>
+        </header>
+        <div class="beg-login-main">
+            <form action="login.html" class="layui-form" method="post">
+                <div class="layui-form-item">
+                    <label class="beg-login-icon">
+                        <i class="layui-icon">&#xe612;</i>
+                    </label>
+                    <input type="text" lay-verify="required" name="name" autocomplete="off" placeholder="这里输入账号" class="layui-input" lay-verType="tips">
+                </div>
+                <div class="layui-form-item">
+                    <label class="beg-login-icon">
+                        <i class="layui-icon">&#xe642;</i>
+                    </label>
+                    <input type="password" lay-verify="required" name="password" autocomplete="off" placeholder="这里输入密码" class="layui-input" lay-verType="tips">
+                </div>
+                  <div class="layui-form-item">
+                    <label class="beg-login-icon">
+                        <i class="layui-icon">&#xe642;</i>
+                    </label>
+                    <input type="password" lay-verify="required" name="password" autocomplete="off" placeholder="这里输入确认密码" class="layui-input" lay-verType="tips">
+                </div>
+                <div class="layui-form-item">
+                    <div class="beg-pull">
+                        <button type="button" class="layui-btn layui-btn-normal" style="width:100%;" lay-submit lay-filter="formDemo">
+                          注册
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
-    @endif
-    <b style="color: red">{{session('msg')}}</b>
-    <div class="lrBox">
-    <div class="form-group">
-    <label for="firstname" class="col-sm-1 control-label">用户名:</label>
-    <div class="col-sm-3">
-      <input type="text" class="form-control" name="admin_name" id="account" 
-           placeholder="请输入用户名">
+        <footer>
+            <p>power by dw © </p>
+        </footer>
     </div>
-  </div>
-  <hr>
-  <div class="form-group">
-    <label for="firstname" class="col-sm-1 control-label">密码:</label>
-    <div class="col-sm-3">
-      <input type="password" class="form-control" name="admin_pwd" id="firstname" 
-           placeholder="请输入密码">
-    </div>
-    </div>
-    <hr>
-    <div class="form-group">
-    <label for="firstname" class="col-sm-1 control-label">确认密码:</label>
-    <div class="col-sm-3">
-      <input type="password" class="form-control" name="admin_pwd1" id="firstname" 
-           placeholder="请确认密码">
-    </div>
-    </div>
-<hr>
-    </div><!--lrBox/-->
-    <div class="lrSub">
-        <input type="submit" class="btn btn-success" id="reg" value="立即注册" />
-    </div>
-</form><!--reg-login/-->
-<div class="height1"></div>
-
-</div><!--maincont-->
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<!-- <script src="/layui/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<!-- <script src="/layui/bootstrap.min.js"></script>
-<script src="/layui/style.js"></script> -->
-<!-- </body>
-</html> -->
-
-<!-- <script src="/static/jquery.js"></script> -->
-<script>
-
-</script>
-@endsection
+    <script type="text/javascript" src="/admin/js/jquery.min.js"></script>
+    <script type="text/javascript" src="./layui/layui.js"></script>
+    <script type="text/javascript" src="/admin/js/login.js"></script>
+</body>
+</html>
